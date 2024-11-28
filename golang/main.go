4d0fddb5e9b3944bb1097dba723ca91007fd4375
sneapppppp <ilyasumarov0100@gmail.com>
2024-11-28 19:47:01 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -1,10 +1,16 @@
+package main
+
 import (
+	"fmt"
 	"isuct.ru/informatics2022/lab4"
 	"isuct.ru/informatics2022/lab6"
 	"isuct.ru/informatics2022/lab7"
 )
 
 func main() {
+	fmt.Println("Hello world")
+	
+	// Вызовы функций внутри тела main
 	lab4.RunLab4Tasks()
 	lab6.RunLab6()
 	lab7.RunLab7()
