709388dffb632b11413d59ba88b0e56fb7c6d82a
sneapppppp <ilyasumarov0100@gmail.com>
2024-12-11 16:40:39 +0300

    Update lab4.go

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -5,23 +5,37 @@ import (
 	"math"
 )
 
-func Calculate(x, a float64) float64 {
+func Calculate(x, a float64) (float64, error) {
+	if x <= 1 {
+		return 0, fmt.Errorf(x)
+	}
+
 	y := math.Pow(a, math.Sqrt(x)-1) - math.Log10(math.Sqrt(x)-1) + math.Pow(math.Sqrt(x)-1, 1.0/3.0)
-	return y
+	return y, nil
 }
 
 func TaskA(b, Xn, Xk, delX float64) []float64 {
 	var y []float64
 	for x := Xn; x <= Xk; x += delX {
-		y = append(y, Calculate(b, x))
+		result, err := Calculate(x, b)
+		if err != nil {
+			fmt.Printf(x, err)
+			continue
+		}
+		y = append(y, result)
 	}
 	return y
 }
 
-func TaskB(b float64, x [5]float64) []float64 {
+func TaskB(b float64, x []float64) []float64 {
 	var y []float64
 	for _, value := range x {
-		y = append(y, Calculate(b, value))
+		result, err := Calculate(value, b)
+		if err != nil {
+			fmt.Printf(, value, err)
+			continue
+		}
+		y = append(y, result)
 	}
 	return y
 }
@@ -29,6 +43,7 @@ func TaskB(b float64, x [5]float64) []float64 {
 func RunLab4() {
 	b := 2.5
 	fmt.Println(TaskA(b, 1.2, 3.7, 0.5))
-	var s = [5]float64{1.28, 1.36, 2.47, 3.68, 4.56}
+
+	var s = []float64{1.28, 1.36, 2.47, 3.68, 4.56}
 	fmt.Println(TaskB(b, s))
 }
