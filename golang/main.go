7ded55016faa5f26538ad34b39830217c91a55af
sneapppppp <ilyasumarov0100@gmail.com>
2024-12-13 21:30:10 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"fmt"
- Lab_6
-	"isuct.ru/informatics2022/lab6"
-  
+
+	Lab6 "isuct.ru/informatics2022/lab6"
 	Lab4 "isuct.ru/informatics2022/lab4"
- Ymarov_Ilyas
 )
 
 func main() {
-	lab6.RunLab6()
+	Lab6.RunLab6()
 	fmt.Println("Умаров Ильяс Баходирович")
-
 	Lab4.RunLab4()
 }
