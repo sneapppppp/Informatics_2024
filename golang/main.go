0112e32764df813ab6a7e8a40a37a0089ede58da
sneapppppp <ilyasumarov0100@gmail.com>
2024-12-17 20:12:58 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 
- 	"isuct.ru/informatics2022/lab6" 
+ 	"isuct.ru/informatics2022/lab4" 
+	"isuct.ru/informatics2022/lab6" 
 	"isuct.ru/informatics2022/lab7" 
 )
 
 func main() {
+	lab4.RunLab4()
 	lab6.RunLab6()
 
 	fmt.Println("Hello world")
