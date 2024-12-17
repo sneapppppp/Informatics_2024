ca54a73eed1d4fe23d59ec0d1ffff4ec0ecf65e6
sneapppppp <ilyasumarov0100@gmail.com>
2024-12-17 20:12:14 +0300

    Update lab7.go

--- golang/lab7/lab7.go
+++ golang/lab7/lab7.go
@@ -4,43 +4,12 @@ import (
 	"fmt"
 )
 
-type Product interface {
-	GetName() string
-	GetPrice() float64
-	ApplyDiscount(discount float64)
-}
-
-type Item struct {
-	Name  string
-	Price float64
-}
-
-func (i *Item) GetName() string {
-	return i.Name
-}
-
-func (i *Item) GetPrice() float64 {
-	return i.Price
-}
-
-func (i *Item) ApplyDiscount(discount float64) {
-	i.Price -= i.Price * discount / 100
-}
-
-func CalculateTotal(products []Product) float64 {
-	total := 0.0
-	for _, product := range products {
-		total += product.GetPrice()
-	}
-	return total
-}
-
 func Executelab7() {
-	item1 := &Item{Name: "Laptop", Price: 1000}
-	item2 := &Item{Name: "Phone", Price: 500}
-	item3 := &Item{Name: "Tablet", Price: 300}
+	laptop := &laptop{Name: "Laptop", Price: 1000}
+	phone := &phone{Name: "Phone", Price: 500}
+	tablet := &tablet{Name: "Tablet", Price: 300}
 
-	products := []Product{item1, item2, item3}
+	products := []Product{laptop, phone, tablet}
 
 	fmt.Printf("Total before discount: %.2f\n", CalculateTotal(products))
 
